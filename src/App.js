f860e56fbb8625d28965d4c17786d448a6be0adc
Matthew Justice <matthewajustice@gmail.com>
2020-12-11 13:55:20 -0500

    FIRST COMMIT IM SORRY. Took a long time to style but I used every minute of it lol

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import styled from 'styled-components';
+import CharacterAccordion from './components/Character';
+import { BASE_URL, PAGE } from './constants';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [charData, setCharData] = useState([]);
+
+  useEffect(() => {
+    for (let i = 1; i < 10; i++) {
+      axios
+        .get(BASE_URL + PAGE + i)
+        .then((res) => {
+          setCharData((charData) => charData.concat(res.data.results));
+        })
+        .catch((err) => console.log('Error: ' + err));
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className='App'>
+      <h1 className='Header'>Star Wars Characters</h1>
+      <CharacterAccordion results={charData} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,65 @@
 // Write your Character component here
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import styled from 'styled-components';
+import { DomainDisabled } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '50%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+export default function CharacterAccordion({ results }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      {results.map((char, index) => (
+        <Accordion key={index}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <StyledNames>
+              <Typography className='names'>{char.name}</Typography>
+            </StyledNames>
+          </AccordionSummary>
+          <AccordionDetails>
+            <dl>
+              <StyledList>
+                <dd>Gender: {char.gender}</dd>
+                <dd>Height: {char.height}</dd>
+                <dd>Skin Color: {char.skin_color}</dd>
+                <dd>Eye Color: {char.eye_color}</dd>
+                <dd>Birth Year: {char.birth_year}</dd>
+              </StyledList>
+            </dl>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
+}
+
+const StyledList = styled.dd`
+  dd {
+    text-align: left;
+    display: list-item;
+    list-style-type: '🌑';
+    padding-inline-start: 1ch;
+  }
+`;
+
+const StyledNames = styled.span`
+  .names {
+    font-weight: bold;
+    font-size: 1rem;
+  }
+`;
